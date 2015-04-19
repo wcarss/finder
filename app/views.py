12d3dce1bda2eb797f9d4c2ee8cfc49284711816
Wyatt Carss <carss.w@gmail.com>
2015-04-19 10:59:13 -0400

    Rewrite finder as a flask app.
    Really, this is just a restructuring. I've used the structure of a
    different project I made called dash, which was probably based off
    of the structure I made for sidechannel, which was itself based off
    of an earlier structure and so on. It leads back to the combined
    advice of Miguel Grinberg[1] and the dozen Flask apps or so I've
    seen at work.
    
    As you can see from the change-list, I mostly pulled the assets
    straight into a static directory, and made index.html into a
    template in that it refers to them using jinja instead of a direct
    url.
    
    At this import, I'm stopping at "it shows pictures and the JS and
    CSS seem to work." The image-posting is not going to work yet --
    I will write the image model next and get it back, then add a list
    of the "best" images so far. And then we'll move on to users and
    personal lists.
    
    1 - http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world

---
+++ app/views.py
@@ -0,0 +1,54 @@
+from app import app
+from flask import render_template
+import datetime
+
+
+@app.route('/')
+def index():
+  current_time = datetime.datetime.now()
+  # dates like "2014-06-21 20:38:15.191355"
+
+  return render_template('index.html', current_time=current_time)
+
+
+@app.route('/quiet')
+def quiet():
+  return render_template('quiet.html')
+
+"""
+@app.route('/
+
+/			find random images
+/users/username		get user data
+/login			post here with username and password to initiate a session
+/logout			post here while logged in to terminate a session
+/best			get here to pull json list of best images
+/mine			get here to pull json list of personal images
+/save			post here to mark an image as good
+/tag			post here to add a tag to an image
+
+
+straight-up rest style
+
+images
+images/id
+users
+users/id
+
+/best -> pulls /images with a sort-by
+/mine -> pull
+
+
+images
+id	url	date	zoom	x	y	x
+
+best
+image_id	count
+
+mine
+image_id	user_id
+
+images
+users
+
+"""
